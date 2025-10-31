2fc65e9abbf97080a7fc85212eae2984abc41955
Rhys-Harris <harrt002@mymail.unisa.edu.au>
2025-10-31 12:24:10 +1030

    Removed instrument name from part text

--- src/soundcontroller/Part.js
+++ src/soundcontroller/Part.js
@@ -1,25 +1,78 @@
+class PartSlider {
+    constructor(name, curValue, lo, hi) {
+        this.name = name;
+        this.value = curValue;
+        this.lo = lo;
+        this.hi = hi;
+    }
+}
+
+// Logical component of the parts found on the DOM soundboard
 export class PartInfo {
     constructor(name, lineNumber, lines) {
+        // E.g., "bassline"
         this.name = name;
+
+        // Where in the original text the instrument is from
         this.lineNumber = lineNumber;
+
         this.muted = false;
 
-        // Now get all part text
-        this.partText = "";
-        let index = this.lineNumber;
+        // All the text related to this intstrument
+        this.getAllText(this.lineNumber, lines);
+        console.log(this.text);
 
+        this.findSliders();
+    }
+
+    // Searches through the source,
+    // finding all text related to this instrument
+    getAllText(index, lines) {
+        this.text = "";
+
+        // Semicolon is the end of instrument
         while (!lines[index].includes(";")) {
-            this.partText += lines[index];
+            this.text += lines[index];
             ++index;
 
-            // Bad part
+            // Bad part (no semicolon)
             if (index >= lines.length) {
                 alert("Instruments should be ended with a semicolon!");
                 return;
             }
         }
-        this.partText += lines[index];
 
-        console.log(this.partText);
+        this.text += lines[index];
+
+        // Remove instrument name at the start
+        const colonIndex = this.text.indexOf(":");
+        if (colonIndex === -1) {
+            alert("Couldn't get sliders for an instrument");
+            return;
+        }
+
+        this.text = this.text.substring(colonIndex+1);
+    }
+
+    // Searches through the text to
+    // dynamically creates new sliders
+    findSliders() {
+        this.sliders = [];
+
+        // EXAMPLE INSTRUMENT
+        // bassline:
+        // note(pick(basslines, bass))
+        // .sound("supersaw")
+        // .postgain(2)
+        // .room(0.6)
+        // .lpf(700)
+        // .room(0.4)
+        // .postgain(pick(gain_patterns, pattern));
+        //
+        // EXPECTED OUTPUT
+        // slider for postgain
+        // slider for room
+        // slider for lpf
+        // slider for room again? (maybe)
     }
 }
