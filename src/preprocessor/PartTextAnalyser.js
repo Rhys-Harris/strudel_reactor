65a328e65b10ddaeb08a8bdf6aa7d70f8a9fba47
Rhys-Harris <harrt002@mymail.unisa.edu.au>
2025-10-31 14:41:39 +1030

    Completed dynamic search for sliders

--- src/preprocessor/PartTextAnalyser.js
+++ src/preprocessor/PartTextAnalyser.js
@@ -4,8 +4,13 @@ import { Node, NODE_CODES } from "./Node";
 
 class PartSlider {
     constructor(name, curValue, lo, hi) {
+        let value = parseFloat(curValue);
+        if (isNaN(value)) {
+            value = 0.0;
+        }
+
         this.name = name;
-        this.value = curValue;
+        this.value = value;
         this.lo = lo;
         this.hi = hi;
     }
@@ -15,14 +20,15 @@ function searchForControls(ast) {
     const controls = [];
 
     const startFunc = ast.children[0];
-    console.log("Finding controls");
     attemptControl(startFunc, controls);
+
+    return controls;
 }
 
 // Recusively searches a function def for possible controls
 function attemptControl(func, controls) {
-    const funcName = func.children[0].text;
-    console.log(funcName);
+    // See if we can use this like a slider
+    attemptCreateSlider(func, controls);
 
     // Check args
     for (let i = 2; i < func.children.length-1; i += 2) {
@@ -39,41 +45,40 @@ function attemptControl(func, controls) {
     }
 }
 
+function attemptCreateSlider(func, controls) {
+    // Too many or too little args (expecting a single one)
+    if (func.children.length !== 4) {
+        return;
+    }
+
+    const arg = func.children[2];
+    
+    // Can't be used on strings or similar, only numbers
+    if (arg.kind !== NODE_CODES.N_NUM) {
+        return;
+    }
+
+    const funcName = func.children[0].text;
+
+    // Add the new slider! (assuming a reasonable range)
+    controls.push(new PartSlider(funcName, arg.text, 0.0, 100.0));
+}
+
 // Takes the text related to an instrument and finds all the controls it
 // can create
 export function GetInstrumentControls(source) {
     const lexer = new PartTextLexer(source);
     const tokens = lexer.lex();
-    console.log(tokens);
     if (tokens.length === 0) {
         return [];
     }
 
     const parser = new PartTextParser(tokens);
     const ast = parser.parse();
-    console.log(ast);
     if (ast.children.length === 0) {
         return [];
     }
 
     const controls = searchForControls(ast);
-    console.log(controls);
     return controls;
-
-
-    // EXAMPLE INSTRUMENT
-    // bassline:
-    // note(pick(basslines, bass))
-    // .sound("supersaw")
-    // .postgain(2)
-    // .room(0.6)
-    // .lpf(700)
-    // .room(0.4)
-    // .postgain(pick(gain_patterns, pattern));
-    //
-    // EXPECTED OUTPUT
-    // slider for postgain
-    // slider for room
-    // slider for lpf
-    // slider for room again? (maybe)
 }
