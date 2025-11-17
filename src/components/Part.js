83e5cd8a7f837208087939716436862c84020a2e
Rhys-Harris <harrt002@mymail.unisa.edu.au>
2025-11-17 17:16:19 +1030

    Instrument names are now truncated

--- src/components/Part.js
+++ src/components/Part.js
@@ -5,10 +5,17 @@ import Slider from "./Slider";
 // It holds all the controls (e.g., sliders, mute)
 
 const Part = ({part, soundBoard}) => {
+    // Ensure name length is reasonable
+    const nameLength = part.name.length;
+    let finalName = part.name;
+    if (nameLength > 5) {
+        finalName = finalName.substring(0, 5);
+    }
+
     return (
-        <div className="col" style={{padding: "3%"}}>
-            <div className="row text-center">
-                <h4 className="form-check-label">{part.name}</h4>
+        <div className="col-2" style={{padding: "3%"}}>
+            <div className="row text-center" style={{height: "15%"}}>
+                <h5 className="form-check-label">{finalName}</h5>
             </div>
             <div className="row">
                 <MuteButton part={part} soundBoard={soundBoard} />
